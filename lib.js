6d94d6017dd6549adfab2f6ea2052f1a66dd76c8
Sonmin <handbear1208@gmail.com>
2023-10-31 10:10:10 +0900

    initial commit

---
+++ lib.js
@@ -0,0 +1,21 @@
+function avg(numbers) {
+    let s = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        s += numbers[i];
+    }
+    return s / numbers.length;
+}
+
+function prime(num) {
+    return false;
+}
+
+function fact(num) {
+    return -1;
+}
+
+module.exports = {
+    avg,
+    prime,
+    fact
+}
